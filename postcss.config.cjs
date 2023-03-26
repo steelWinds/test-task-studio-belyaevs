9e5a46b0872bcb228b87662ddcaa8405c2c8888c
steelWinds <kirillsurov0@gmail.com>
2023-03-26 20:49:30 +0900

    update(app): some updates

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     'postcss-preset-env': {
-      features: { 'nesting-rules': false },
-    },
+      features: { 'nesting-rules': false }
+    }
   },
 }
